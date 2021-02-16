428657cb709155f76cd68e07fe25cf51df81df4a
Delirius - Daniel Herren <github@delirius.ch>
2021-02-16 10:49:28 +0100

    add language

--- src/Resources/contao/languages/en/modules.php
+++ src/Resources/contao/languages/en/modules.php
@@ -4,7 +4,7 @@
 /**
  * Front end modules
  */
-$GLOBALS['TL_LANG']['FMD']['page_inherit_module'] = array('Seitenfeld mit Vererbung', '');
+$GLOBALS['TL_LANG']['FMD']['page_inherit_module'] = array('Page field with inheritance', '');
 
 $GLOBALS['TL_LANG']['tl_module']['pageinherit_template'] = array('Template','Choose template. (Template name starts with pageinherit_)');
 
--- src/Resources/contao/languages/en/tl_page.php
+++ src/Resources/contao/languages/en/tl_page.php
@@ -1,10 +1,9 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_page']['pageinherit_legend'] = 'Seitenfelder mit Vererbung';
+$GLOBALS['TL_LANG']['tl_page']['pageinherit_legend'] = 'Page fields with inheritance';
 
-$GLOBALS['TL_LANG']['tl_page']['pageinheritcssclass'] = array('CSS-Klasse mit Vererbung', 'CSS-Klasse wird auf die Unterseiten vererbt und automatisch im &lt;body&gt; als Klasse hinzugefügt');
-$GLOBALS['TL_LANG']['tl_page']['pageinheritvalue'] = array('Eigener Wert mit Vererbung
-', 'Wert wird auf die Unterseiten vererbt und wird mit dem Modul «Seitenfeld mit Vererbung» hinzugefügt. So kann beispielsweise pro Stamm ein eigenes .css geladen werden.');
+$GLOBALS['TL_LANG']['tl_page']['pageinheritcssclass'] = array('CSS class with inheritance', 'CSS class is inherited to the subpages and automatically added as a class in the &lt;body&gt;.');
+$GLOBALS['TL_LANG']['tl_page']['pageinheritvalue'] = array('Value with inheritance', 'Value is inherited to the subpages and is added using the «Page Field with Inheritance» module. For example, a separate .css can be loaded per root.');
 
 
 ?>
